5ff2d59f9d09fffa17ab251af96a4cec0ec91fbb
Michael Guo <michaelguo@meta.com>
2026-03-24 22:50:54 -0700

    style: switch to Helvetica Neue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en" className="scroll-smooth">
       <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Sora:wght@300;400;500;600;700&display=swap"
-          rel="stylesheet"
-        />
+
       </head>
       <body className="antialiased">{children}</body>
     </html>
